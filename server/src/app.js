f604fca710414207ca25c8a5fa3cc930360433eb
uchemukolo <uchek7@gmail.com>
2017-11-26 01:59:02 +0100

    Version all APIs

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import event from './route/index';
+import center from './route/index';
+
 
 const app = express();
 
@@ -10,7 +12,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/api/', event);
+app.use('/api/v1', event);
+app.use('/api/v1', center);
 
 app.get('/', (req, res) => res.status(200).send({
   message: 'Welcome to our first route!!!'
