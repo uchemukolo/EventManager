3228ad819d08d6ae568c36b1ce9e4d9f8deb8c2b
uchemukolo <uchek7@gmail.com>
2017-11-26 15:14:07 +0100

    update static files

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import event from './route/index.js';
+import center from './route/index.js';
 
 
 const app = express();
