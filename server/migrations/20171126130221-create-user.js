9c12cf4437f7404b58be5ba2ced9fc58d6cc0bf2
uchemukolo <uchek7@gmail.com>
2017-11-26 15:15:09 +0100

    create user model and correcponding migration

---
+++ server/migrations/20171126130221-create-user.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      LastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      Email: {
+        type: Sequelize.STRING,
+        isEmail: true,
+        allowNull: false,
+        unique: true
+      },
+      Password: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
