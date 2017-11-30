fca90f1a00cf6f32dbf0421988c6e4e05799b9a8
uchemukolo <uchek7@gmail.com>
2017-11-30 01:08:23 +0100

    add new validations

--- server/src/middlewares/Validations.js
+++ server/src/middlewares/Validations.js
@@ -1,60 +1,105 @@
 const Validation = {
-    centerId(req, res, next){
-        const centerId = req.params.centerId;
-    
-        if (!recipeId) {
-          return res.status(400).json({
-            message: 'Parameter must be a number!'
-          });
-        }
-        next();
-      },
-    
-    userId(req, res, next){
-        const userId = req.params.userId;
-    
-        if (!userId) {
-          return res.status(400).json({
-            message: 'Parameter must be a number!'
-          });
-        }
-        next();
-      },
-    signUp(req, res, next) {
-        const { username, email, password, confirmPassword } = req.body;
-        if (!username || typeof username !== 'string') {
-          return res.status(400).json({
-            username: 'Please Enter Username'
-          });
-        } else if (!email || typeof email !== 'string') {
-          return res.status(400).json({
-            email: 'Please Enter Email'
-          });
-        } else if (!password || typeof  password !== 'string') {
-          return res.status(400).json({
-            password: 'Please Enter password'
-          });
-        } else if (password.length < 6) {
-          return res.status(400).json({
-            password: 'Password is too short!'
-          });
-        } else if(password !== confirmPassword) {
-          return res.status(400).json({
-            password: 'password does not match'
-            });
-        }  next();
-    },
-    signIn(req, res, next) {
-        const { username, password } = req.body;
-        if (!username || typeof username !== 'string') {
-        res.status(400).json({
-            message: 'Please enter Your username or email'
-         });
-        } else if (!password || typeof password !== 'string') {
-        res.status(400).json({
-        message: 'Please enter Your Password'
-        });
-        } next();
+  centerId(req, res, next) {
+    const centerId = req.params.centerId;
+    if (!recipeId) {
+      return res.status(400).send({
+        message: 'Parameter must be a number!'
+      });
     }
-    }; 
-    export default Validation;
+    next();
+  },
+
+  userId(req, res, next) {
+    const userId = req.params.userId;
+    if (!userId) {
+      return res.status(400).send({
+        message: 'Parameter must be a number!'
+      });
+    }
+    next();
+  },
+
+  signup(req, res, next) {
+    const { 
+username, firstName, lastName, email, password, confirmPassword 
+} = req.body;
+    if (!username) {
+      return res.status(400).send({
+        username: 'Please Enter Username'
+      });
+    } else if (!email) {
+      return res.status(400).send({
+        email: 'Please Enter Email'
+      });
+    } else if (!firstName) {
+      return res.status(400).send({
+        email: 'Please Enter Your First Name'
+      });
+    } else if (!lastName) {
+      return res.status(400).send({
+        email: 'Please Enter Your Last Name'
+      });
+    } else if (!password) {
+      return res.status(400).send({
+        password: 'Please Enter password'
+      });
+    } else if (password.length < 6) {
+      return res.status(400).send({
+        password: 'Password is too short!'
+      });
+    } else if (password !== confirmPassword) {
+      return res.status(400).send({
+        password: 'password does not match'
+      });
+    } next();
+  },
+  signin(req, res, next) {
+    const { username, password } = req.body;
+    if (!username) {
+      res.status(400).send({
+        message: 'Please enter Your username or email'
+      });
+    } else if (!password) {
+      res.status(400).send({
+        message: 'Please enter Your Password'
+      });
+    } next();
+  },
+  addEvent(req, res, next) {
+    const { eventType, eventDate } = req.body;
+    if (!eventType) {
+      res.status(400).send({
+        message: 'Please Select Type Of Event'
+      });
+    } else if (!eventDate) {
+      res.status(400).send({
+        message: 'Please Choose a Date'
+      });
+    } next();
+  },
+  addCenter(req, res, next) {
+    const { name, description, location, capacity, venueType, } = req.body;
+    if (!name) {
+      res.status(400).send({
+        message: 'Please Add Name Of The Center!'
+      });
+    } else if (!description) {
+      res.status(400).send({
+        message: 'Field Cannot Be Empty!'
+      });
+    } else if (!location) {
+      res.status(400).send({
+        message: 'Please Select location Of Event Center'
+      });
+    } else if (!capacity) {
+      res.status(400).send({
+        message: 'Please Select Capacity'
+      });
+    } else if (!venueType) {
+      res.status(400).send({
+        message: 'Please Select Venue Type'
+      });
+    } next();
+  }
+};
+export default Validation;
