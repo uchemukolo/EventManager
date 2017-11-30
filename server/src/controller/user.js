b201607714c9f6e8d3e041138c8c2ebe78eadbfb
uchemukolo <uchek7@gmail.com>
2017-11-30 01:07:52 +0100

    update user controller

--- server/src/controller/user.js
+++ server/src/controller/user.js
@@ -7,88 +7,77 @@ require('dotenv').config();
 
 const Users = model.Users;
 class User {
-    signup (req, res) {
+    signup(req, res){
         const {email, username, firstName, lastName, password, confirmPassword} = req.body;
-
-            Users.find({
-                where:{
-                    $or: [
-                        { email: email },
-                        { username: username },
-                    ]
+        Users.find({
+            where:{
+                $or: [
+                    { email: email },
+                    { username: username }
+                  ]
                 }
-            }).then(foundUser => {
+              })
+              .then(foundUser => {
                 if (!foundUser) {
-                    return Users
-                    .create({
-                        email: email,
-                        username: username,
-                        firstName: firstName,
-                        lastName: lastName,
-                        password: bcrypt.hashSync(password, 10),
-                        role: 'Regular'
-                    }).then(newUser => {
-                        res.status(201).send({
-                            message: 'Signup Successful',
-                            newUser
+                  return Users.create({
+                    email: email,
+                    username: username,
+                    firstName: firstName,
+                    lastName: lastName,
+                    password: bcrypt.hashSync(password, 10),
+                    role: 'Regular'
+                  })
+                  .then(newUser => {
+                    res.status(201).send({
+                       message: 'Signup Successful',
+                       email: email,
+                       username: username,
+                       firstName: firstName,
+                       lastName: lastName
+                      })
                     })
                     .catch(err => {
-                        console.log(err)
-                        res.status(500).send({
-                            message: 'some error occured!'
-                        });
-                    })
-                
+                      res.status(500).send({
+                        message: 'some error occured!'
+                      });
+                    });
+                  }
+                })
+                .catch(err => {
+                  res.status(500).send({
+                    message: 'some error occured!'
+                  });
                 });
-                 } 
-                // else if (foundUser.email) {
-                //     res.status(400).send({
-                //         message: 'email already exists'
-                //     })
-                // } else if (foundUser.username) {
-                //     res.status(400).send({
-                //         message: 'user name already exists'
-                //     })
-                // }
-            })
-    }
-    signin (req, res) {
-        const { username, email, password, } = req.body;
-        // if (!username || typeof username !== 'string') {
-        //     res.status(400).send({
-        //         message: 'Please enter Your username or email'
-        //      });
-        //     } else if (!password || typeof password !== 'string') {
-        //     res.status(400).send({
-        //     message: 'Please enter Your Password'
-        //     });
-        //     } else {
-        Users.findOne({
-                 where: {
-                    $or: [
-                        { username: req.body.username },
-                        { email: req.body.username }
-                     ]
-                 }
-             }).then(foundUser => { 
-                 if (!foundUser) {
-                     res.status(400).send({
-                         message: 'Username or Password!'
-                     })
-                 } else if(bcrypt.compareSync(req.body.password, foundUser.password)){ 
-                    if (foundUser) {
-                        return res.status(200).send({
-                             message: 'Signin Successful!',
-                         }) 
-                         } else {
-                        res.status(401).send({
-                            Error: 'Incorrect Password'
-                            })
-                     };
-                };
-             });
-        };
-    }        
-const userController = new User();
+              }
 
+      signin (req, res) {
+        const { username, email, password, } = req.body;
+        Users.findOne({
+          where: {
+            $or: [
+              { username: req.body.username },
+              { email: req.body.username }
+            ]
+          }
+        })
+        .then(foundUser => {
+          if (!foundUser) {
+            res.status(400).send({
+              message: 'Username or Password!'
+            })
+          } else if(bcrypt.compareSync(req.body.password, foundUser.password)){
+            if (foundUser) {
+              return res.status(200).send({
+                message: 'Signin Successful!',
+              })
+            } else {
+              res.status(401).send({
+                Error: 'Incorrect Password'
+              })
+            };
+          };
+        });
+      };
+    }
+const userController = new User();
 export default userController;
