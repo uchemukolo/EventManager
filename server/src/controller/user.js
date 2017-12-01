7368f2c0a37cdb1f90c702f9d59ae95eab73166f
uchemukolo <uchek7@gmail.com>
2017-12-01 14:37:13 +0100

    refactor user signup controller

--- server/src/controller/user.js
+++ server/src/controller/user.js
@@ -1,5 +1,5 @@
 import model from '../../models';
-import bcrypt from 'bcryptjs'
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import Sequelize from 'sequelize';
 
@@ -7,89 +7,85 @@ require('dotenv').config();
 
 const Users = model.Users;
 class User {
-    signup(req, res){
-        const {email, username, firstName, lastName, password, confirmPassword} = req.body;
-        Users.find({
-            where:{
-                $or: [
-                    { email: email },
-                    { username: username }
-                  ]
-                }
-              })
-              .then(foundUser => {
-                if (!foundUser) {
-                  return Users.create({
-                    email: email,
-                    username: username,
-                    firstName: firstName,
-                    lastName: lastName,
-                    password: bcrypt.hashSync(password, 10),
-                    role: 'Regular'
-                  })
-                  .then(newUser => {
-                    res.status(201).send({
-                       message: 'Signup Successful',
-                       email: email,
-                       username: username,
-                       firstName: firstName,
-                       lastName: lastName
-                      })
-                    })
-                    .catch(err => {
-                      res.status(500).send({
-                        message: 'some error occured!'
-                      });
-                    });
-                  }
-                })
-                .catch(err => {
-                  res.status(500).send({
-                    message: 'some error occured!'
-                  });
-                });
-              }
-
-      signin (req, res) {
-        const { username, email, password, } = req.body;
-        Users.findOne({
-          where: {
-            $or: [
-              { username: req.body.username },
-              { email: req.body.username }
-            ]
-          }
-        })
-        .then(foundUser => {
-          if (!foundUser) {
-            res.status(400).send({
-              message: 'Incorrect Signin Credentials!'
-            })
-          } else if(bcrypt.compareSync(req.body.password, foundUser.password)){
-            const token = jwt.sign({user: foundUser}, process.env.SECRET_KEY, {
+  signup(req, res) {
+    const {email, username, firstName, lastName, password, confirmPassword} = req.body;
+    Users.find({
+      where: {
+        $or: [
+          { email },
+          { username }
+        ]
+      }
+    })
+      .then((foundUser) => {
+        if (!foundUser) {
+          return Users.create({
+            email,
+            username,
+            firstName,
+            lastName,
+            password: bcrypt.hashSync(password, 10),
+            role: 'Regular'
+          })
+            .then((newUser) => { 
+              const token = jwt.sign({ user: foundUser }, process.env.SECRET_KEY, {
               expiresIn: 60 * 60 * 24
             });
-            console.log(foundUser.role)
-            if (foundUser.role === 'Admin') {
-              return res.status(200).send({
-                message: 'Welcome Admin',
-                role: foundUser.role,
+              res.status(201).send({
+                message: 'Signup Successful',
+                username,
+                firstName,
+                lastName,
+                email,
                 Token: token
-              })  
-            }else{
-              return res.status(200).send({
-                message: 'Signin Successful!',
-                Token: token
-                
-              })
-            }
-          }else {
-            res.status(401).send({
-              Error: 'Incorrect Password'
+              });
             })
-          };
+            .catch((err) => {
+              res.status(500).send({
+                message: 'some error occured!'
+              });
+            });
+        }
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: 'some error occured!'
         });
-      };
+      });
+  }
+
+  signin(req, res) {
+    const { username, email, password, } = req.body;
+    Users.findOne({
+      where: {
+        $or: [
+          { username: req.body.username },
+          { email: req.body.username }
+        ]
+      }
+    })
+      .then((foundUser) => {
+        if (!foundUser) {
+          res.status(400).send({
+            message: 'Incorrect Signin Credentials!'
+          });
+        }
+        else if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+          const token = jwt.sign({ user: foundUser }, process.env.SECRET_KEY, {
+            expiresIn: 60 * 60 * 24
+          });
+          return res.status(200).send({
+            message: 'Signin Successful!',
+            Token: token
+          });
+        }
+        else {
+          res.status(401).send({
+            Error: 'Incorrect Password'
+          });
+        }
+        });
+  }
     }
 const userController = new User();
 export default userController;
